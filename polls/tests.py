63c42bb70854f096e60048bd9604e749cce47d54
thewritingstew <richard.o.stewart@gmail.com>
2018-09-11 20:53:35 -0600

    add tests for future and past question Detail view [iss010]

--- polls/tests.py
+++ polls/tests.py
@@ -111,3 +111,28 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_question_list'],
                 ['<Question: Past question 1.>',
                 '<Question: Past question 2.>'])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with pub_date in the future returns
+        a 404 not found response from the server.
+        """
+        future_question = create_question('Future question.', 1)
+        # reverse('polls:detail', args = (question.id,))
+        url = reverse('polls:detail', args = (future_question.id, ))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        the detail view of a question with a pub_date in the past returns
+        a 200 status code from the server and displays the question's text.
+        """
+        # setup
+        past_question = create_question('Past question.', -1)
+        url = reverse('polls:detail', args = (past_question.id, ))
+        response = self.client.get(url)
+        # tests
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
