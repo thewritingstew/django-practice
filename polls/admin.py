4c41d1646e722b7f3588195cf371bb1bcb9dbd0e
thewritingstew <richard.o.stewart@gmail.com>
2018-09-15 08:33:16 -0600

    add choices inline with question [iss #22]

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    """Sets up the admin for choices to be inline with the Question the
+    choice belongs to, and shows 3 extra slots for choices.
+    """
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    This sets up the admin view of the question model with fieldsets to
+    group differet aspects of the question.
+    """
+    fieldsets = [
+        (None,          {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+# Register models
+admin.site.register(Question, QuestionAdmin)
